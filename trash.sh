16a4ad0a817be70f31972a77a0415a927abca214
Oliverfeng1 <863472527@qq.com>
2019-12-25 21:21:19 +0800

    add trash.sh

---
+++ trash.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#回收站
+alias rm='trash'
+alias rl='trashlist'
+
+LocalTrash="$HOME/.local/share/Trash/files"
+trash()
+{
+    [ ! -d $LocalTrash ] && mkdir -p $LocalTrash
+    mv -b $@ $LocalTrash
+}
+trashlist()
+{
+    echo -e "================= Garbage Lists in Trash ================="
+    echo -e "\a"
+    echo -e "\aUse 'cleartrash' to clear all garbages in Trash!!!"
+    ls -l $LocalTrash
+}
+#清空回收站
+cleartrash()
+{
+    echo -ne "\aClear all garbages in Trash, Sure?"
+    read confirm
+    if [ $confirm == 'y' -o $confirm == 'Y' ] ;then
+        /bin/rm -rf $LocalTrash/* &> /dev/null
+        /bin/rm -rf $LocalTrash/.* &> /dev/null
+    fi
+}
